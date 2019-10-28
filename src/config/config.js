28fbb25969aaad74b31d50899c2f1361b3c0c4c2
David Muhanguzi <38077368+MuhanguziDavid@users.noreply.github.com>
2019-10-28 12:35:12 +0300

    - use env variable for database details (#5)
    - change config.json to config.js

---
+++ src/config/config.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+  "development": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_TEST,
+    "host": process.env.HOST,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "postgres",
+    "url": process.env.DATABASE_URL,
+    "operatorsAliases": false
+  }
+}
