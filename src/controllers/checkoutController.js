c39dfcae0298e7b674848b637eb9ab8c52c11757
David Muhanguzi <38077368+MuhanguziDavid@users.noreply.github.com>
2020-01-21 12:21:24 +0300

    fix checkout endpoint (#19)
    - ensure that one cannot checkout more items than is available

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -16,12 +16,20 @@ class CheckoutController {
       if (!foundItem) {
         return Errors.errorHandler(res, 404, 'Item does not exist');
       }
+      const newItemQuantity = foundItem.dataValues.quantity - Number(req.body.quantity)
+      console.log('foundItem', foundItem.dataValues);
+      if (newItemQuantity < 0) {
+        return Errors.errorHandler(
+          res,
+          404,
+          `Only ${foundItem.dataValues.quantity} items of this type left in the store, please select fewer items for checkout`
+        );
+      }
       const checkoutData = {
         ...req.body,
         itemId: req.params.id
       }
       const checkedoutItem = await models.checkout.create(checkoutData);
-      const newItemQuantity = foundItem.dataValues.quantity - Number(req.body.quantity)
       const newBody = {
         quantity: newItemQuantity
       }
