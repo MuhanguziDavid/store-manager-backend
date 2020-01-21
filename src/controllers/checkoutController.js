7073be798844fdcb0ef49e5831ba5967d7189099
David Muhanguzi <38077368+MuhanguziDavid@users.noreply.github.com>
2020-01-21 15:27:51 +0300

    add editing option for item checkout (#20)

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -17,7 +17,6 @@ class CheckoutController {
         return Errors.errorHandler(res, 404, 'Item does not exist');
       }
       const newItemQuantity = foundItem.dataValues.quantity - Number(req.body.quantity)
-      console.log('foundItem', foundItem.dataValues);
       if (newItemQuantity < 0) {
         return Errors.errorHandler(
           res,
@@ -39,6 +38,50 @@ class CheckoutController {
         success: true,
         message: 'Item checked out successfully',
         checkedoutItem: checkedoutItem,
+      })
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async editCheckout(req, res, next) {
+    try {
+      const checkoutId = req.params.id;
+      const foundCheckout = await models.checkout.findByPk(checkoutId);
+      if (!foundCheckout) {
+        return Errors.errorHandler(res, 404, 'Record does not exist');
+      }
+
+      const foundItem = await models.item.findAll({
+        where: {
+          id: foundCheckout.dataValues.itemId,
+        },
+        returning: true,
+        plain: true
+      });
+      const revertPreviousQuanity = foundItem.dataValues.quantity + foundCheckout.dataValues.quantity;
+      const newItemQuantity = revertPreviousQuanity - Number(req.body.quantity);
+      if (newItemQuantity < 0) {
+        return Errors.errorHandler(
+          res,
+          404,
+          `Only ${foundItem.dataValues.quantity} items of this type left in the store, please select fewer items for checkout`
+        );
+      }
+
+      const editedCheckout = await models.checkout.update(req.body,
+        { where: { id: checkoutId }, returning: true, plain: true });
+
+      const newBody = {
+        quantity: newItemQuantity
+      }
+      await models.item.update(newBody,
+        { where: { id: foundCheckout.dataValues.itemId }, returning: true, plain: true });
+
+      return res.status(200).json({
+        success: true,
+        message: 'Edited successfully',
+        checkedoutItem: editedCheckout[1],
       })
     } catch (error) {
       next(error);
--- src/routes/checkoutRoute.js
+++ src/routes/checkoutRoute.js
@@ -4,5 +4,6 @@ import CheckoutController from '../controllers/checkoutController';
 const router = express.Router();
 
 router.post('/:id/checkout', CheckoutController.checkoutItem);
+router.put('/:id/checkout/:id', CheckoutController.editCheckout);
 
 export default router;
